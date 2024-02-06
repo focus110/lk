1758568fc05fb71b5d16abf96f7b273aa5dc2309
focus110 <iamemmaochuko@gmail.com>
2024-02-06 23:15:00 +0100

    align clips image center

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -127,11 +127,13 @@ export default function ProjectsPage() {
         </div>
 
         <div className="relative overflow-hidden w-full pb-14 ">
-          <Image
-            src={clipsGIF}
-            alt="Clips projects"
-            className="mx-auto sm:m-10 w-[300px] h-[300px] relative z-[2] ml-0 md:ml-[150px]"
-          />
+          <div className="flex w-[50%] justify-center">
+            <Image
+              src={clipsGIF}
+              alt="Clips projects"
+              className="mx-auto sm:m-10 w-[300px] h-[300px] relative z-[2]"
+            />
+          </div>
 
           <div className="absolute z-[1] bottom-0 left-1/2 -translate-x-1/2 bg-[#00FF00] w-[200px] md:w-[150px] h-[110rem] md:h-full"></div>
 
